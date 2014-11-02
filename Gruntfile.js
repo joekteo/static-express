7fcf623bdb053feb016cdc3172d49fc976c63aa6
Joe Teo <revelate@live.com>
2014-11-01 18:36:43 -0700

    add jscs, jshint, gitignore files

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+  module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      all: ['*.js']
+    },
+    jscs: {
+      all: {
+        options: {
+          'standard': 'airbnb'
+        },
+        files: {
+  src: ['app.js'] // Target-specific file/directory goes here.
+}
+}
+}
+});
+  grunt.registerTask('test', ['jshint', 'jscs']);
+  grunt.registerTask('default', ['test']);
+};
